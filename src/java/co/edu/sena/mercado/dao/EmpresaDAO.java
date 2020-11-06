56f270315a4a4ee24803ac9c1fc4a1f31b78c093
Cerber0Z <57652183+Cerber0Z@users.noreply.github.com>
2020-11-06 12:35:41 -0500

    commit raro
    commit raro

--- src/java/co/edu/sena/mercado/dao/EmpresaDAO.java
+++ src/java/co/edu/sena/mercado/dao/EmpresaDAO.java
@@ -7,59 +7,287 @@ package co.edu.sena.mercado.dao;
 
 import co.edu.sena.mercado.dto.empresaDTO;
 import co.edu.sena.mercado.util.Conexion;
-import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  *
- * @author equipo
+ * @author DELL
  */
-public class EmpresaDAO {
-    
-    private Connection conn = null;
-    private PreparedStatement ps = null;
-    private ResultSet rs = null;
-    
-    public EmpresaDAO(Connection conn) {
-        this.conn = conn;
-    }
-    
-     public boolean registroEmpresa(empresaDTO empresaDTO, int idUsuario) throws MySQLIntegrityConstraintViolationException, SQLException {
-        String consulta = "INSERT INTO empresa( esEmpresa, direccionEmpresa, telefonoEmpresa, celularEmpresa, correoEmpresa, idCiudadFK, idUsuarioFK) VALUES "
-                + "(?,?,?,?,?,?,?)";
+public class empresaDAO {
+
+    Connection cn;
+    PreparedStatement ps;
+    ResultSet rs;
+    Conexion con = new Conexion();
+    String consulta;
+    empresaDTO empresaDTO = new empresaDTO();
+    ArrayList<empresaDTO> listaEmpresa = new ArrayList<>();
+
+    public boolean registroEmpresa(empresaDTO empresaDTO, int idUsuario) {
+        con = new Conexion();
+        consulta = "INSERT INTO empresa( esEmpresa, nombreEmpresa, direccionEmpresa, telefonoEmpresa, celularEmpresa, correoEmpresa, idCiudadFK, idUsuarioFK) VALUES (?,?,?,?,?,?,?,?)";
         try {
-            ps = conn.prepareStatement(consulta);
+            cn = con.getConnection();
+            ps = cn.prepareStatement(consulta);
             ps.setInt(1, empresaDTO.getEsEmpresa());
-            ps.setString(2, empresaDTO.getDirEmpresa());
-            ps.setString(3, empresaDTO.getTelEmpresa());
-            ps.setString(4, empresaDTO.getCelEmpresa());
-            ps.setString(5, empresaDTO.getCorreoEmpresa());
-            ps.setInt(6, empresaDTO.getIdCiudad());
-            ps.setInt(7, empresaDTO.getIdUsuario());
+            ps.setString(2, empresaDTO.getNombreEmpresa());
+            ps.setString(3, empresaDTO.getDirEmpresa());
+            ps.setString(4, empresaDTO.getTelEmpresa());
+            ps.setString(5, empresaDTO.getCelEmpresa());
+            ps.setString(6, empresaDTO.getCorreoEmpresa());
+            ps.setInt(7, empresaDTO.getIdCiudad());
+            ps.setInt(8, empresaDTO.getIdUsuario());
             ps.executeUpdate();
             System.out.println("..... registro de empresa realizado consulta " + ps.toString());
             //cerrarCon(ps, cn, rs);
             return true;
 
-        } catch (MySQLIntegrityConstraintViolationException ex) {
-            System.out.println(ex);
-            throw new MySQLIntegrityConstraintViolationException();     
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("xxxxxxxxxxxxxxxxxxx error al registrar empresa " + e);
             System.out.println("xxxxxxxxxxxxxxxxxxx consulta " + ps.toString());
             // cerrarCon(ps, cn, rs);
-            throw new SQLException();
-        } 
+            return false;
+        } finally {
+            Conexion.close(cn);
+            Conexion.close(ps);
+            Conexion.close(rs);
+        }
 
     }
-     
-         public void CloseAll(){
-          Conexion.close(conn);
-          Conexion.close(ps);
-          Conexion.close(rs);
+
+    public boolean actualizarEmpresa(empresaDTO empresaDTO, int idUsuario) {
+        con = new Conexion();
+        consulta = "UPDATE empresa SET esEmpresa=? ,nombreEmpresa=?,direccionEmpresa=?,telefonoEmpresa=?,celularEmpresa=?,correoEmpresa=?,idCiudadFK=? WHERE idUsuarioFK=?";
+        try {
+            cn = con.getConnection();
+            ps = cn.prepareStatement(consulta);
+            ps.setInt(1, empresaDTO.getEsEmpresa());
+            ps.setString(2, empresaDTO.getNombreEmpresa());
+            ps.setString(3, empresaDTO.getDirEmpresa());
+            ps.setString(4, empresaDTO.getTelEmpresa());
+            ps.setString(5, empresaDTO.getCelEmpresa());
+            ps.setString(6, empresaDTO.getCorreoEmpresa());
+            ps.setInt(7, empresaDTO.getIdCiudad());
+            ps.setInt(8, idUsuario);
+            ps.executeUpdate();
+            System.out.println("..... actualizacion de empresa realizado consulta " + ps.toString());
+            //cerrarCon(ps, cn, rs);
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("xxxxxxxxxxxxxxxxxxx error al actualizar empresa " + e);
+            System.out.println("xxxxxxxxxxxxxxxxxxx consulta " + ps.toString());
+            // cerrarCon(ps, cn, rs);
+            return false;
+        } finally {
+            Conexion.close(cn);
+            Conexion.close(ps);
+            Conexion.close(rs);
+        }
+
     }
+
+    public boolean actualizarDatosFaltantes(empresaDTO empresaDTO, int idUsuario) {
+        con = new Conexion();
+        consulta = "UPDATE empresa SET direccionEmpresa=?,telefonoEmpresa=?,celularEmpresa=? WHERE idUsuarioFK=?";
+        try {
+            cn = con.getConnection();
+            ps = cn.prepareStatement(consulta);
+            ps.setString(1, empresaDTO.getDirEmpresa());
+            ps.setString(2, empresaDTO.getTelEmpresa());
+            ps.setString(3, empresaDTO.getCelEmpresa());
+            ps.setInt(4, idUsuario);
+            ps.executeUpdate();
+            System.out.println("..... actualizacion de empresa realizado consulta " + ps.toString());
+            //cerrarCon(ps, cn, rs);
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("xxxxxxxxxxxxxxxxxxx error al actualizar empresa " + e);
+            System.out.println("xxxxxxxxxxxxxxxxxxx consulta " + ps.toString());
+            // cerrarCon(ps, cn, rs);
+            return false;
+        } finally {
+            Conexion.close(cn);
+            Conexion.close(ps);
+            Conexion.close(rs);
+        }
+
+    }
+
+    public empresaDTO buscarEmpresa(int idUsuario) {
+        con = new Conexion();
+        consulta = "SELECT * FROM empresa WHERE idUsuarioFK = ? limit 1";
+        try {
+            cn = con.getConnection();
+            ps = cn.prepareStatement(consulta);
+            ps.setInt(1, idUsuario);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                empresaDTO = new empresaDTO();
+                empresaDTO.setIdEmpresa(rs.getInt("idEmpresa"));
+                empresaDTO.setEsEmpresa(rs.getInt("esEmpresa"));
+                empresaDTO.setNombreEmpresa(rs.getString("nombreEmpresa"));
+                empresaDTO.setDirEmpresa(rs.getString("direccionEmpresa"));
+                empresaDTO.setTelEmpresa(rs.getString("telefonoEmpresa"));
+                empresaDTO.setCelEmpresa(rs.getString("celularEmpresa"));
+                empresaDTO.setCorreoEmpresa(rs.getString("correoEmpresa"));
+                empresaDTO.setIdCiudad(rs.getInt("idCiudadFK"));
+                empresaDTO.setIdUsuario(rs.getInt("idUsuarioFK"));
+
+            }
+            System.out.println(".....  empresa encontrada consulta " + ps.toString());
+            System.out.println(".....  empresa encontrada  " + empresaDTO.toString());
+            //cerrarCon(ps, cn, rs);
+            return empresaDTO;
+
+        } catch (SQLException e) {
+            System.out.println("xxxxxxxxxxxxxxxxxxx error al buscar empresa " + e);
+            System.out.println("xxxxxxxxxxxxxxxxxxx consulta " + ps.toString());
+            // cerrarCon(ps, cn, rs);
+            return null;
+        } finally {
+            Conexion.close(cn);
+            Conexion.close(ps);
+            Conexion.close(rs);
+        }
+
+    }
+
+    public ArrayList<empresaDTO> listarEmpresasFiltro() {
+        con = new Conexion();
+        listaEmpresa = new ArrayList<>();
+        consulta = "SELECT idEmpresa, nombreEmpresa, esEmpresa "
+                + "FROM empresa WHERE esEmpresa = 1";
+        try {
+            cn = con.getConnection();
+            ps = cn.prepareStatement(consulta);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                empresaDTO = new empresaDTO();
+                empresaDTO.setIdEmpresa(rs.getInt("idEmpresa"));
+                empresaDTO.setNombreEmpresa(rs.getString("nombreEmpresa"));
+                listaEmpresa.add(empresaDTO);
+
+            }
+            //cerrarCon(ps, cn, rs);
+            return listaEmpresa;
+
+        } catch (SQLException e) {
+            System.out.println("xxxxxxxxxxxxxxxxxxx error al buscar empresa " + e);
+            System.out.println("xxxxxxxxxxxxxxxxxxx consulta " + ps.toString());
+            // cerrarCon(ps, cn, rs);
+            return null;
+        } finally {
+            Conexion.close(cn);
+            Conexion.close(ps);
+            Conexion.close(rs);
+        }
+
+    }
+
     
+    public ArrayList<empresaDTO> listarEmpresas() {
+        con = new Conexion();
+        listaEmpresa = new ArrayList<>();
+        consulta = "SELECT * FROM empresa WHERE esEmpresa = 1 ORDER by rand() LIMIT 5";
+        try {
+            cn = con.getConnection();
+            ps = cn.prepareStatement(consulta);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                empresaDTO = new empresaDTO();
+                empresaDTO.setIdEmpresa(rs.getInt("idEmpresa"));
+                empresaDTO.setEsEmpresa(rs.getInt("esEmpresa"));
+                empresaDTO.setNombreEmpresa(rs.getString("nombreEmpresa"));
+                empresaDTO.setDirEmpresa(rs.getString("direccionEmpresa"));
+                empresaDTO.setTelEmpresa(rs.getString("telefonoEmpresa"));
+                empresaDTO.setCelEmpresa(rs.getString("celularEmpresa"));
+                empresaDTO.setCorreoEmpresa(rs.getString("correoEmpresa"));
+                empresaDTO.setIdCiudad(rs.getInt("idCiudadFK"));
+                empresaDTO.setIdUsuario(rs.getInt("idUsuarioFK"));
+                listaEmpresa.add(empresaDTO);
+
+            }
+
+            //cerrarCon(ps, cn, rs);
+            return listaEmpresa;
+
+        } catch (SQLException e) {
+            System.out.println("xxxxxxxxxxxxxxxxxxx error al buscar empresa " + e);
+            System.out.println("xxxxxxxxxxxxxxxxxxx consulta " + ps.toString());
+            // cerrarCon(ps, cn, rs);
+            return null;
+        } finally {
+            Conexion.close(cn);
+            Conexion.close(ps);
+            Conexion.close(rs);
+        }
+
+    }
+
+    public empresaDTO buscarEmpresaXProducto(String idProducto) {
+        con = new Conexion();
+        consulta = "SELECT emp.*,ciud.nombreCiudad FROM empresa emp INNER JOIN ciudad ciud ON ciud.idCiudad=emp.idCiudadFK WHERE idEmpresa = ?";
+        try {
+            cn = con.getConnection();
+            ps = cn.prepareStatement(consulta);
+            ps.setString(1, idProducto);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                empresaDTO = new empresaDTO();
+                empresaDTO.setIdEmpresa(rs.getInt("idEmpresa"));
+                empresaDTO.setEsEmpresa(rs.getInt("esEmpresa"));
+                empresaDTO.setNombreEmpresa(rs.getString("nombreEmpresa"));
+                empresaDTO.setDirEmpresa(rs.getString("direccionEmpresa"));
+                empresaDTO.setTelEmpresa(rs.getString("telefonoEmpresa"));
+                empresaDTO.setCelEmpresa(rs.getString("celularEmpresa"));
+                empresaDTO.setCorreoEmpresa(rs.getString("correoEmpresa"));
+                empresaDTO.setIdCiudad(rs.getInt("idCiudadFK"));
+                empresaDTO.setIdUsuario(rs.getInt("idUsuarioFK"));
+                empresaDTO.setNombreCiudad(rs.getString("nombreCiudad"));
+
+            }
+            System.out.println(".....  empresa encontrada consulta " + ps.toString());
+            System.out.println(".....  empresa encontrada  " + empresaDTO.toString());
+            //cerrarCon(ps, cn, rs);
+            return empresaDTO;
+
+        } catch (SQLException e) {
+            System.out.println("xxxxxxxxxxxxxxxxxxx error al buscar empresa " + e);
+            System.out.println("xxxxxxxxxxxxxxxxxxx consulta " + ps.toString());
+            // cerrarCon(ps, cn, rs);
+            return null;
+        } finally {
+            Conexion.close(cn);
+            Conexion.close(ps);
+            Conexion.close(rs);
+        }
+
+    }
+
+//    public void cerrarCon(PreparedStatement ps, Connection con, ResultSet rs) {
+//        try {
+//            if (con != null) {
+//                con.close();
+//            }
+//            if (ps != null) {
+//                ps.close();
+//            }
+//            if (rs != null) {
+//                rs.close();
+//            }
+//
+//            // System.out.println("conexxion cerrada");
+//        } catch (SQLException e) {
+//            System.out.println("error al cerrar conexion " + e);
+//        }
+//
+//    }
 }
--- src/java/co/edu/sena/mercado/dao/UsuarioDAO.java
+++ src/java/co/edu/sena/mercado/dao/UsuarioDAO.java
@@ -5,59 +5,216 @@
  */
 package co.edu.sena.mercado.dao;
 
-import co.edu.sena.mercado.dto.usuarioDTO;
+import co.edu.sena.mercado.dto.*;
 import co.edu.sena.mercado.util.Conexion;
-import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.SQLException; 
+import java.util.ArrayList;
 
 /**
  *
- * @author equipo
+ * @author DELL
  */
-public class UsuarioDAO {
-    private Connection conn = null;
-    private PreparedStatement ps = null;
-    private ResultSet rs = null;
-    
-    public UsuarioDAO(Connection conn) {
-        this.conn = conn;
-    }
+public class usuarioDAO {
 
-    public int registroUsuario(usuarioDTO datosUsu) throws Exception, MySQLIntegrityConstraintViolationException {
-        int idUsuario = 0;
-        String consulta = "INSERT INTO usuario(emailusuario, passwordUsuario, fechaPassword, estadoUsuario, fkRol,codActivacion) VALUES (?,md5(?), now(),?,?,md5(?))";
+    Conexion con = new Conexion();
+    Connection cn;
+    PreparedStatement ps;
+    ResultSet rs;
+    String consulta;
+    usuarioDTO usuarioDTO = new usuarioDTO();
+    ArrayList<usuarioDTO> listaUsuario = new ArrayList<>();
+
+    public boolean registroUsuario(usuarioDTO datosUsu) {
+        con = new Conexion();
+        consulta = "INSERT INTO usuario(emailusuario, passwordUsuario, fechaPassword, estadoUsuario, fkRol,codActivacion) VALUES (?,md5(?), now(),?,?,md5(?))";
         try {
-            ps = conn.prepareStatement(consulta, Statement.RETURN_GENERATED_KEYS);
+            cn = con.getConnection();
+            ps = cn.prepareStatement(consulta);
             ps.setString(1, datosUsu.getCorreoUsu());
             ps.setString(2, datosUsu.getClaveUsu());
             ps.setString(3, datosUsu.getEstadoUsu());
             ps.setInt(4, datosUsu.getIdRol());
             ps.setString(5,datosUsu.getCodigo());
-            System.out.println(ps.toString());
             ps.executeUpdate();
-            rs = ps.getGeneratedKeys();
-            if (rs.next()) {
-                idUsuario = rs.getInt(1);
-            }
-            return idUsuario;
-        }catch (MySQLIntegrityConstraintViolationException ex) {
-            System.out.println(ex);
-            throw new MySQLIntegrityConstraintViolationException();     
-        }catch (SQLException e) {
-            System.out.println("........error al relizar el registro registroUsuario " + e);
-            System.out.println("........ consulta " + ps.toString());
-            throw new Exception();
+            System.out.println("registro usuario realizado consulta " + ps.toString());
+            return true;
+        } catch (SQLException e) {
+            System.out.println("error al registrar usuario " + e);
+            System.out.println("consulta " + ps.toString());
+            return false;
+       }finally{
+            Conexion.close(cn);
+            Conexion.close(ps);
+            Conexion.close(rs);
+        }
+    }
+    public boolean actualizarUsuario(usuarioDTO datosUsu) {
+        con = new Conexion();
+        consulta = "UPDATE usuario SET emailusuario=?,passwordUsuario=md5(?),fechaPassword=now() WHERE idUsuario=?";
+        try {
+            cn = con.getConnection();
+            ps = cn.prepareStatement(consulta);
+            ps.setString(1, datosUsu.getCorreoUsu());
+            ps.setString(2, datosUsu.getClaveUsu());
+            ps.setInt(3, datosUsu.getIdUsuario());
+            
+            ps.executeUpdate();
+           
+            return true;
+        } catch (SQLException e) {
+            System.out.println("error al registrar usuario " + e);
+            System.out.println("consulta " + ps.toString());
+            return false;
+        }finally{
+            Conexion.close(cn);
+            Conexion.close(ps);
+            Conexion.close(rs);
         }
     }
     
-         public void CloseAll(){
-          Conexion.close(conn);
-          Conexion.close(ps);
-          Conexion.close(rs);
+    
+       public usuarioDTO login(usuarioDTO usuario) {
+
+        try {
+            con = new Conexion();
+            String sql = "SELECT * FROM usuario WHERE emailUsuario = ? AND passwordUsuario"
+                    + " = md5(?) AND estadoUsuario = 1 limit 1";
+            PreparedStatement ps = con.getConnection().prepareStatement(sql);
+            ps.setString(1, usuario.getCorreoUsu());
+            ps.setString(2, usuario.getClaveUsu());
+            ResultSet rs = ps.executeQuery();
+            usuarioDTO usua = new usuarioDTO();
+            while (rs.next()) {
+
+                usua.setIdUsuario(rs.getInt("idUsuario"));
+                usua.setCorreoUsu(rs.getString("emailUsuario"));
+                usua.setEstadoUsu(rs.getString("estadoUsuario"));
+                usua.setIdRol(rs.getInt("fkRol"));
+                usua.setNumIngreso(rs.getInt("numeroIngreso"));
+
+            }
+            return usua;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }finally{
+            Conexion.close(cn);
+            Conexion.close(ps);
+            Conexion.close(rs);
+        }
+
     }
     
+    
+    
+    public usuarioDTO buscarUsuario(String correo, String clave) {
+        con = new Conexion();
+     
+        
+        consulta="select * from usuario where passwordUsuario=md5(?) and  emailusuario=?";
+        try {
+            cn = con.getConnection();
+            ps = cn.prepareStatement(consulta);
+            ps.setString(1, clave);
+            ps.setString(2, correo);
+            rs= ps.executeQuery();
+            while(rs.next()){
+            usuarioDTO=new usuarioDTO();
+            usuarioDTO.setCorreoUsu(rs.getString("emailusuario"));
+            usuarioDTO.setEstadoUsu(rs.getString("estadoUsuario"));
+            usuarioDTO.setFechaClave(rs.getString("fechaPassword"));
+            usuarioDTO.setIdRol(rs.getInt("fkRol"));
+            usuarioDTO.setIdUsuario(rs.getInt("idUsuario"));
+            usuarioDTO.setCodigo(rs.getString("codActivacion"));
+            usuarioDTO.setNumIngreso(rs.getInt("numeroIngreso"));
+           
+            
+            }
+           
+            System.out.println("usuario encontrado consulta " + ps.toString());
+            System.out.println("... usuario " + usuarioDTO.toString());
+            return usuarioDTO;
+        } catch (SQLException e) {
+            System.out.println("error al consultar  usuario " + e);
+            System.out.println("consulta " + ps.toString());
+            return null;
+         }finally{
+            Conexion.close(cn);
+            Conexion.close(ps);
+            Conexion.close(rs);
+        }
+    }
+   
+    public boolean activarUsuario(String correo, String codigo) {
+        con = new Conexion();
+        consulta="UPDATE usuario SET estadoUsuario=1 WHERE codActivacion=md5(?) and emailusuario=? AND estadoUSUARIO = 0";
+        try {
+            cn = con.getConnection();
+            ps = cn.prepareStatement(consulta);
+            ps.setString(1, codigo);
+            ps.setString(2, correo);
+            int count = ps.executeUpdate();
+            return  (count > 0);
+        } catch (SQLException e) {
+            System.out.println("error al activar  usuario " + e);
+            System.out.println("consulta " + ps.toString());
+            return false;
+        
+         }finally{
+            Conexion.close(cn);
+            Conexion.close(ps);
+            Conexion.close(rs);
+        }
+    }
+    public boolean actEnteda(int numIngreso, int id) {
+        con = new Conexion();
+     
+        
+        consulta="UPDATE usuario SET numeroIngreso=?  WHERE idUsuario=?";
+        try {
+            cn = con.getConnection();
+            ps = cn.prepareStatement(consulta);
+            ps.setInt(1, numIngreso);
+            ps.setInt(2, id);
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("error al actualizar numero de ingreso usuario " + e);
+            System.out.println("consulta " + ps.toString());
+            return false;
+         }finally{
+            Conexion.close(cn);
+            Conexion.close(ps);
+            Conexion.close(rs);
+        }
+    }
+    public boolean eliminarUsuario(String correo, String clave) {
+        con = new Conexion();
+     
+        
+        consulta="delete from usuario where passwordUsuario=md5(?) and  emailusuario=?";
+        try {
+            cn = con.getConnection();
+            ps = cn.prepareStatement(consulta);
+            ps.setString(1, clave);
+            ps.setString(2, correo);
+            ps.executeUpdate();
+           
+           
+            System.out.println("usuario eliminado consulta " + ps.toString());
+          
+            return true;
+        } catch (SQLException e) {
+            System.out.println("error al eliminar  usuario " + e);
+            System.out.println("consulta " + ps.toString());
+            return false;
+         }finally{
+            Conexion.close(cn);
+            Conexion.close(ps);
+            Conexion.close(rs);
+        }
+    }
 }
